a6f7624c74ee87cd2c4a5d038355900aceedf933
Anuj Jamwal <anuj.jamwal1@gmail.com>
2014-03-28 14:10:05 +0530

    |User Interface| Styling software list page

--- app/helpers/softwares_helper.rb
+++ app/helpers/softwares_helper.rb
@@ -1,6 +1,11 @@
 module SoftwaresHelper
   MAP = {1 => 'Freeware'}
+
   def download_policy(software)
     MAP.fetch(software.download_policy_id, 'Licensed')
+  end
+
+  def download_count(software)
+    "#{software.download_count > 0 ? software.download_count : 'No'} Downloads"
   end
 end
