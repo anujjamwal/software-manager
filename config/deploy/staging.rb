f12fbac0534c9752a587137a81df0ed5226cc1db
Anuj Jamwal <anuj.jamwal1@gmail.com>
2014-06-17 12:28:51 +0530

    update ruby to 2.1.1

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :stage, 'staging'
 set :rails_env, 'staging'
 
-set :rvm_ruby_version, '2.0.0@soft-manager'
+set :rvm_ruby_version, '2.1.1@soft-manager'
 
 role :app, %w{10.15.1.94}
 role :sync, %w{10.15.1.94}
